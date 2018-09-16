626db94e31b6a0c88a4ca6b0e7d4ffbf06b97844
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-09-16 20:20:56 +0200

    Implement annotation for mapping context to controller classes

---
+++ src/main/java/annotations/WebContext.java
@@ -0,0 +1,12 @@
+package annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface WebContext {
+    String value() default "/"; //default, used when there is only one arg not assigned to any name;
+}
